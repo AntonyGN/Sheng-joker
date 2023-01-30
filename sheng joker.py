001b056c7ad7db0bcf3034238bb26a11700a4fdf
AntonyGN <antonygithendu@gmail.com>
2023-01-30 16:52:12 +0300

    first commit

---
+++ sheng joker.py
@@ -0,0 +1,17 @@
+import random
+
+jokes = [
+   "Wasee wa vitz wanadai gava introduce petrol ya sachets.",
+    "bei ya iPhone ireduce ju mtu alishabite hio apple.",
+    "Miguna Miguna anyang’anywe jina moja wapatie wenyewe hawana majina kama Nameless.",
+    "Ile light inakuanga at the end of the tunnel izimwe ku save energy.",
+    "PK irudi 4bob",
+    "Elections zifanywe kwa FB na Twitter ndio ikuwe cheap. Balance wapatie masufferer.",
+    "kama fegi ni 7 bob ambia shopkeeper aivute mpaka ifike ya 5 bob akuuzie",
+    "tissue paper itolewe the second sheet. inakuwanga ya nini by the way?",
+    "Siku za mwizi ziongezwe zikue ka 68 ju kumekauka.",
+    "Ni DSTV tutalipia ama ni rent?",
+    "KBC ionyeshe game za Euros.",
+]
+
+print(random.choice(jokes))
